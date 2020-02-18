229b716824d5c09ecc44efceeb74525cadc369f4
jojowoodo <jojowoodo@yahoo.com>
2020-02-18 12:59:58 +0200

    La till en getroute method

--- SlutProjekt 1/src/com/company/Node.java
+++ SlutProjekt 1/src/com/company/Node.java
@@ -50,31 +50,42 @@ public class Node {
         return neighbours;
     }
 
-    public static double getDistance(double lon1, double lat1, double lon2, double lat2)
-    {
-        lon1 = lon1*Math.PI/180.0;
-        lat1 = lat1*Math.PI/180.0;
-        lon2 = lon2*Math.PI/180.0;
-        lat2 = lat2*Math.PI/180.0;
+    public static double getDistance(double lon1, double lat1, double lon2, double lat2) {
+        lon1 = lon1 * Math.PI / 180.0;
+        lat1 = lat1 * Math.PI / 180.0;
+        lon2 = lon2 * Math.PI / 180.0;
+        lat2 = lat2 * Math.PI / 180.0;
 
 
         double dlon = lon2 - lon1;
         double dlat = lat2 - lat1;
-        double a = Math.pow(Math.sin(dlat/2), 2) + Math.cos(lat1) * Math.cos(lat2) *        Math.pow(Math.sin(dlon/2), 2);
-        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        double a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
         double km = 6367 * c;
 
         return km;
     }
 
 
-    public  double calculateH(Node destination){
+    public double calculateH(Node destination) {
         double H;
-       H = getDistance(destination.getLongitude(),destination.getLatitude(),getLongitude(),getLatitude());
+        H = getDistance(destination.getLongitude(), destination.getLatitude(), getLongitude(), getLatitude());
         System.out.println(H);
         return H;
     }
 
+    public getRoute(Node source, destination) {
+        ArrayList candidates, visited = new ArrayList();
+        Node current = source;
+        boolean done;
 
+        if (done = false) {
+            int minF = 0;
+            char next = null;
+
+
+        }
+
+    }
 
 }
